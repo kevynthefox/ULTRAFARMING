c315a586e87cd79f3fff492d6aaf1f318626134b
kevynthefox <carmodykevyn6@gmail.com>
2026-05-08 09:55:30 -0400

    started working on a way to update the position of the crops via list

--- Assets/scripts/crops/cropGrowth.cs
+++ Assets/scripts/crops/cropGrowth.cs
@@ -13,8 +13,10 @@ public class cropGrowth : MonoBehaviour
 
     public List<GameObject> segments;
     public int current_segment;
+    public List<cropSegment> segments_data;
 
     public GameObject next_point;
+    public GameObject previous_point;
     public Quaternion procedural_rotation;
 
     public void Start()
@@ -26,27 +28,36 @@ public class cropGrowth : MonoBehaviour
             {
                 var bottom = Instantiate(bottom_section, transform.position, transform.rotation);
                 next_point = bottom.transform.Find("next_point").gameObject;
+                previous_point = bottom;
                 segments.Add(bottom);
             }
-
-            procedural_rotation = new Quaternion(transform.rotation.x, transform.rotation.y + (i * 90f),
-                transform.rotation.z, transform.rotation.w);
-            
-            var middle = Instantiate(middle_section,next_point.transform.position,procedural_rotation ); //the i * 90 is to make it turn more each segment
-            next_point = middle.transform.Find("next_point").gameObject;
-            segments.Add(middle);
-
-            if (i == maxGrowth)
+            else
             {
-                var top = Instantiate(top_section, transform.position, procedural_rotation);
-                segments.Add(top);
-                next_point = null;;
+                if (i == maxGrowth)
+                {
+                    var top = Instantiate(top_section, transform.position, procedural_rotation);
+                    segments.Add(top);
+                    next_point = null;;
+                }
+                else
+                {
+                    procedural_rotation = new Quaternion(transform.rotation.x, transform.rotation.y + (i * 90f),
+                        transform.rotation.z, transform.rotation.w);
+            
+                    var middle = Instantiate(middle_section,previous_point.transform.position,procedural_rotation ); //the i * 90 is to make it turn more each segment
+                    next_point = middle.transform.Find("next_point").gameObject;
+                    previous_point = middle;
+                    segments.Add(middle);
+                }
             }
+           
+
+            
         }
 
         foreach (var segment in segments)
-        {
-            segment.transform.localScale = new Vector3(segment.transform.localScale.x, 0, segment.transform.localScale.z);
+        { 
+            //add a thing to add it to the segments data list thingy here
             segment.SetActive(false);
             //shrink them down until they're invisible so that they can grow, then turn them off to save processing power
         }
@@ -61,4 +72,45 @@ public class cropGrowth : MonoBehaviour
     }
 
     
+}
+
+
+[Serializable]
+public class cropSegment
+{
+    
+    public GameObject segment;
+    public GameObject _next_point;
+    public GameObject _previous_point;
+
+    [SerializeField]
+    private int _dist_percentage;
+
+    public float distance;
+
+
+    void calculate_distance()
+    {
+        distance = Vector3.Distance(_previous_point.transform.position,_next_point.transform.position);
+        dist_percentage = 0;
+    }
+    
+    public int dist_percentage 
+    {
+        
+        get
+        {
+            return _dist_percentage;
+        }
+        set
+        {
+            _dist_percentage = value;
+
+            segment.transform.position = Vector3.Lerp(_previous_point.transform.position, _next_point.transform.position, (dist_percentage / 100));// previous_point.transform.position + (distance * (dist_percentage / 100));
+        }
+        
+        
+        
+    }
+
 }
