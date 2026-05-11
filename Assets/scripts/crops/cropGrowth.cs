5ba1b4a6c9d45b427578209ac49c3a9c3a1ec45d
kevynthefox <carmodykevyn6@gmail.com>
2026-05-11 17:20:48 -0400

    continued working on crop growth. current issue: appearently missing an object when trying to set the segment in the list

--- Assets/scripts/crops/cropGrowth.cs
+++ Assets/scripts/crops/cropGrowth.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 
 public class cropGrowth : MonoBehaviour
 {
+    [Header("Growth data")]
     public int maxGrowth;
-    public int currentGrowth;
+    public int growth_rate;
+    
+    [Header("sections")]
     public GameObject bottom_section;
     public GameObject middle_section;
     public GameObject top_section;
 
+    [Header("segments")]
     public List<GameObject> segments;
     public int current_segment;
     public List<cropSegment> segments_data;
 
+    [Header("points")]
     public GameObject next_point;
     public GameObject previous_point;
     public Quaternion procedural_rotation;
@@ -28,7 +33,10 @@ public class cropGrowth : MonoBehaviour
             {
                 var bottom = Instantiate(bottom_section, transform.position, transform.rotation);
                 next_point = bottom.transform.Find("next_point").gameObject;
-                previous_point = bottom;
+                
+                bottom.transform.parent = this.transform;
+                add_segment_to_crop_segments(i,bottom);
+                previous_point = this.gameObject;
                 segments.Add(bottom);
             }
             else
@@ -37,7 +45,11 @@ public class cropGrowth : MonoBehaviour
                 {
                     var top = Instantiate(top_section, transform.position, procedural_rotation);
                     segments.Add(top);
+                    top.transform.parent = previous_point.transform;//this makes it so all segments above the previous one move up at once, so that the whole plant can actually grow instead of collecting in one spot
+                    add_segment_to_crop_segments(i,top);
                     next_point = null;;
+                    
+                    
                 }
                 else
                 {
@@ -46,8 +58,12 @@ public class cropGrowth : MonoBehaviour
             
                     var middle = Instantiate(middle_section,previous_point.transform.position,procedural_rotation ); //the i * 90 is to make it turn more each segment
                     next_point = middle.transform.Find("next_point").gameObject;
+                    middle.transform.parent = previous_point.transform;//this makes it so all segments above the previous one move up at once, so that the whole plant can actually grow instead of collecting in one spot
+                    add_segment_to_crop_segments(i,middle);
                     previous_point = middle;
                     segments.Add(middle);
+                    
+                    
                 }
             }
            
@@ -57,6 +73,7 @@ public class cropGrowth : MonoBehaviour
 
         foreach (var segment in segments)
         { 
+            
             //add a thing to add it to the segments data list thingy here
             segment.SetActive(false);
             //shrink them down until they're invisible so that they can grow, then turn them off to save processing power
@@ -66,12 +83,23 @@ public class cropGrowth : MonoBehaviour
     public void step_growth()
     {
         segments[current_segment].SetActive(true);
-        //play an animation here
-        
-        current_segment++;
+        segments_data[current_segment].dist_percentage += growth_rate;
+
+        if (segments_data[current_segment].dist_percentage >= 100)
+        {
+            current_segment++;  //after current segment has finished growing, tick the next segment
+        }
     }
 
-    
+    private void add_segment_to_crop_segments(int i, GameObject segment)
+    {
+        //using insert because with that, you can insert a null, and then populate it. I think...
+        segments_data.Insert(i , null);
+        segments_data[i]._segment = segment;
+        segments_data[i]._next_point = next_point; //dont need to worry about getting these from the sepcific object since next point and previous point are global in this script. as in, when set by one object, they are set like that for all of them ig?
+        segments_data[i]._previous_point = previous_point;
+        segments_data[i].dist_percentage = 0;
+    }
 }
 
 
@@ -79,7 +107,7 @@ public class cropGrowth : MonoBehaviour
 public class cropSegment
 {
     
-    public GameObject segment;
+    public GameObject _segment;
     public GameObject _next_point;
     public GameObject _previous_point;
 
@@ -106,7 +134,7 @@ public class cropSegment
         {
             _dist_percentage = value;
 
-            segment.transform.position = Vector3.Lerp(_previous_point.transform.position, _next_point.transform.position, (dist_percentage / 100));// previous_point.transform.position + (distance * (dist_percentage / 100));
+            _segment.transform.position = Vector3.Lerp(_previous_point.transform.position, _next_point.transform.position, (dist_percentage / 100));// previous_point.transform.position + (distance * (dist_percentage / 100));
         }
         
         
