72205d9302c0daaa4b79de80447a9701ced3bb70
kevynthefox <carmodykevyn6@gmail.com>
2026-06-20 17:03:30 -0400

    started work on the weapon wheel

---
+++ Assets/scripts/player/UI/weapon_wheel_button_controller.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class weapon_wheel_script : MonoBehaviour
+{
+    public int id;
+
+    public string itemName;
+    public TextMeshProUGUI itemText;
+    //public Image selectedItem;
+    private bool selected = false;
+    //public Sprite icon;
+
+    public weapon_wheel_controller controller;
+    
+    void Update()
+    {
+        if (selected) //should probably change this to not use update later
+        {
+            //selectedItem.sprite = icon;
+            itemText.text = itemName;
+        }
+    }
+
+    public void Selected()
+    {
+        selected = true;
+    }
+    public void Deselected()
+    {
+        selected = false;
+    }
+
+    public void HoverEnter()
+    {
+        itemText.text = itemName;
+        controller.selected_weapon = id;
+    }
+    
+    public void HoverExit()
+    {
+        itemText.text = "";
+    }
+
+}
---
+++ Assets/scripts/player/UI/weapon_wheel_controller.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class weapon_wheel_controller : MonoBehaviour
+{
+    public GameObject wheel;
+    public bool wheel_open;
+    public FirstPersonLook look;
+    public List<GameObject> weapons;
+
+    public int selected_weapon;
+    
+    public void toggle_wheel(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 1)
+        {
+            wheel.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0.0001f;
+            look.enabled = false;
+        }
+        else
+        {
+            wheel.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Time.timeScale = 1;
+            look.enabled = true;
+        }
+    }
+
+    public void equip_weapon()
+    {
+
+        foreach (GameObject weapon in weapons)
+        {
+            if (weapons.IndexOf(weapon) != selected_weapon) weapon.SetActive(false);
+        }
+
+        if (!weapons[selected_weapon].activeSelf)
+        {
+            weapons[selected_weapon].SetActive(true);
+        }
+    }
+    
+    
+}
