d46496452c88f09214c64beacafb6b5a45f6166e
kevynthefox <carmodykevyn6@gmail.com>
2026-07-21 13:09:41 -0400

    got the logic of the fencing around the playable area to work, now i just need it to have a tileable 3d fence texture so i dont need to stretch it or make a fence model that is placed repeatedly

--- Assets/scripts/other/expand_land.cs
+++ Assets/scripts/other/expand_land.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using TMPro;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,12 +11,12 @@ public class expand_land : MonoBehaviour
     public int current_expansion_level;
 
     public money_holder moneyHolder;
-    
-    public InfiniteTerrain infinite_terrain;
 
     public TextMeshProUGUI cost_display;
 
-    public Rigidbody rb;
+    public InfiniteTerrain terrain;
+    
+    public GameObject border_1, border_2,border_3,border_4;
     
     public void expand()
     {
@@ -23,41 +24,25 @@ public class expand_land : MonoBehaviour
         {
             moneyHolder.money_update(-expansion_cost);
 
-            infinite_terrain.worldSettings.renderDistance++;
-
-            expansion_cost += (
-                ((infinite_terrain.worldSettings.renderDistance+1) * (infinite_terrain.worldSettings.renderDistance+1))
-                -
-                ((infinite_terrain.worldSettings.renderDistance) * (infinite_terrain.worldSettings.renderDistance))
-            );
             current_expansion_level++;
+            
+            expansion_cost = (
+                math.square(current_expansion_level+1) - math.square(current_expansion_level)
+            );
             cost_display.text = "$" + expansion_cost + "<br> expansion level: " + (current_expansion_level+1) +"x"+(current_expansion_level+1);
 
-
-
-            //this.AddComponent<Rigidbody>();
-            //rb = GetComponent<Rigidbody>();
-            rb.isKinematic = false;
-            rb.linearVelocity = new Vector3(infinite_terrain.worldSettings.chunkSize*current_expansion_level, 0, infinite_terrain.worldSettings.chunkSize*current_expansion_level);
-
-            //infinite_terrain.ShutDown();
-            infinite_terrain.OnStart();
-
-            StartCoroutine(turn_off_velocity());
-            //transform.position =  new Vector3(0, 26, 0);
+            int chunkSize = terrain.worldSettings.chunkSize+1;
+            
+            border_1.transform.localScale = new Vector3(current_expansion_level * chunkSize, 1, 1);
+            border_2.transform.localScale = new Vector3(1, 1, current_expansion_level * chunkSize);
+            border_3.transform.localScale = new Vector3(current_expansion_level * chunkSize, 1, 1);
+            border_4.transform.localScale = new Vector3(1, 1, current_expansion_level * chunkSize);
+            
+            border_1.transform.position = new Vector3((current_expansion_level * chunkSize) / 2, 26, current_expansion_level * chunkSize);
+            border_2.transform.position = new Vector3(current_expansion_level * chunkSize, 26, (current_expansion_level * chunkSize) / 2);
+            border_3.transform.position = new Vector3((current_expansion_level * chunkSize) / 2, 26, 1);
+            border_4.transform.position = new Vector3(1, 26, (current_expansion_level * chunkSize) / 2);
         }
-    }
-
-    public IEnumerator turn_off_velocity()
-    {
-        yield return new WaitForSeconds(3f);
-        rb.linearVelocity = rb.linearVelocity =
-            new Vector3(-infinite_terrain.worldSettings.chunkSize * current_expansion_level, 0,
-                -infinite_terrain.worldSettings.chunkSize * current_expansion_level);
-        yield return new WaitForSeconds(3);
-        rb.isKinematic = true;
-        transform.position = new Vector3(0, 26, 0);
-        transform.rotation = Quaternion.identity;
     }
 
 }
